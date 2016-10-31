139831b216eb7868b36bd7fbf688f834ba7a05da
Garrett Boggs and Matt Nicholas <matt@>
2016-10-31 16:45:22 -0700

    add queue check

--- app/components/new-ticket.js
+++ app/components/new-ticket.js
@@ -1,19 +1,39 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  allStepsTaken: false,
+  gotPairHelp: false,
+  spentTime: false,
+  helpSent: false,
+
   currentTime: Ember.computed(function() {
-    return moment().format('dddd');
+    return moment().format();
   }),
 
   actions: {
     saveTicket() {
+      if(this.get('name') && this.get('content')){
+        this.set('helpSent', true);
+
       var params = {
         name: this.get('name'),
         location: this.get('location'),
         content: this.get('content'),
         time: this.get('currentTime')
       };
+      this.set('helpSent', true);
       this.sendAction('saveTicket', params);
+ 
+    },
+
+    setStepsTaken(){
+      this.set('allStepsTaken', true);
+    },
+    setPairHelp(){
+      this.set('gotPairHelp', true);
+    },
+    setSpentTime(){
+      this.set('spentTime', true);
     }
   }
 });
